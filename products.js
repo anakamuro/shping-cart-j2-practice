fa0b711706070810f5b16d3641da338a4f343194
Akihiko Nakamura <anakamuro@earthlink.net>
2023-11-17 16:30:48 -0500

    commit

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+    {
+        id: 0,
+        name: "T-shirt 1",
+        price: 29.99, 
+        instock: 100, 
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        imgSrc: "https://i5.walmartimages.com/seo/C2-Sport-Performance-T-Shirt-in-Columbia-Blue-L-5100_4a515d73-6929-4a8e-ac95-7c09526d3f8e.c91a37d43502e1813019e08cda733668.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF"
+
+    }, 
+    {
+        id: 1,
+        name: "T-shirt 2",
+        price: 24.99, 
+        instock: 43, 
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        imgSrc: "https://i5.walmartimages.com/seo/Wonder-Nation-Boys-Short-Sleeve-Kid-Tough-T-Shirt-Sizes-4-18-Husky_b2f22f0c-6747-460b-9180-05a307860908.93e94034b20e188b1910cb1b725cfd29.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF"
+
+    }, 
+    {
+        id: 2,
+        name: "T-shirt 3",
+        price: 19.99, 
+        instock: 10, 
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        imgSrc: "https://i5.walmartimages.com/seo/Wonder-Nation-Boys-Short-Sleeve-Striped-T-Shirt-Sizes-4-18-Husky_53c7f474-8fa7-4177-b09b-6ebc33f73fb8.6f9ef8f04e1ec3fb2e4d400fe99003e3.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF"
+
+    }, 
+    {
+        id: 3,
+        name: "T-shirt 4",
+        price: 25.99, 
+        instock: 5, 
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        imgSrc: "https://i5.walmartimages.com/seo/Sports-Grey-Shirt-for-Men-Gildan-2000-Men-T-Shirt-Cotton-Men-Shirt-Original-Men-s-Shirts-Best-Mens-Classic-Short-Sleeve-Tee_83877d36-2cca-41a2-b416-ce48759c0f5e.02f1f4ebe54f2b42858d5dba3a3c497e.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF"
+
+    }, 
+    {
+        id: 4,
+        name: "T-shirt 5",
+        price: 29.99, 
+        instock: 4, 
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        imgSrc: "https://i5.walmartimages.com/seo/Black-T-shirts-for-Men-Gildan-2000-Men-Shirt-Cotton-Men-Shirts-Mens-Value-Shirts-Best-Mens-Classic-Short-Sleeve-T-shirt_492531f4-6b6c-4aa9-9522-b1d81c2bc493.c7360b4097810e7eede3606cd218764b.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF"
+
+    }, 
+    {
+        id: 5,
+        name: "T-shirt 6",
+        price: 39.99, 
+        instock: 40, 
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        imgSrc: "https://i5.walmartimages.com/seo/Daisy-T-Shirts-for-Men-Gildan-2000-Men-Shirt-Cotton-Yellow-Men-Tees-Men-s-Value-Shirts-Best-Mens-Classic-Short-Sleeve-T-shirt_f3c21660-1ac0-459c-bf87-451e6f09d334.9421b401403d51bf060bb2a061bdb791.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF"
+
+    }, 
+]
